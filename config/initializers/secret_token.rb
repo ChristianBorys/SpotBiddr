97b1e1fe3d26f170aa68421fbf73960aba54aa0e
Christian Borys <christianborys@gmail.com>
2013-10-11 18:24:29 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Spotbiddr::Application.config.secret_key_base = 'd666d16f4d71a35d0204dd7b75faceaa5c39dc87b943f3162edc27022ccb3c0e98c04f19b6010583a399522470dd9609e693187cec0f6076ed7a76de4e13e9c6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Spotbiddr::Application.config.session_store :cookie_store, key: '_spotbiddr_session'
