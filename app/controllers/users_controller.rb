07283cbef87fa1d58f5d6a3e025f2f393119ae1a
Christian Borys <christianborys@gmail.com>
2013-10-11 23:44:02 -0400

    generated welcome controller and built partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,39 @@
 class UsersController < ApplicationController
+  
+  def index
+    @user = User.all
+  end
+
   def show
+    @user = User.find(params[:id])
   end
 
   def new
+    @user = User.new
   end
 
   def edit
+    @user = User.find(params[:id])
   end
+
+  def create
+    @user = User.new
+    
+    if @user.save
+          auto_login(@user)
+          redirect_to root_path, :notice => "Account Created"
+    else
+      render :new
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Spotbiddr::Application.routes.draw do
   
   resources :users
-  
+
+  root 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
